73b91b9227870758f6397ac98de37163c4f85b16
Abobakr <abobakrms.rm@gmail.com>
2026-05-16 01:51:51 +0300

    SQL 50 DONE

---
+++ SQL/SQL_50/Subqueries/185_Department_Top_Three_Salaries.sql
@@ -0,0 +1,14 @@
+SELECT
+	Department,
+	Employee,
+	Salary
+FROM(
+	SELECT
+		d.name AS Department,
+		e.name AS Employee,
+		e.salary AS Salary,
+		DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS SalaryRank
+	FROM Employee AS e
+	JOIN Department AS d ON e.departmentId = d.id
+) AS RankedSalaries
+WHERE SalaryRank <= 3
